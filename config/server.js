f2a3292ed7ce6257818a47eed0e73a73835b1078
AndreNicolasCordeiro <andrenicolascordeiro@gmail.com>
2022-04-19 15:59:30 -0300

    reactavancado-api

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'db58affb81d993b2aae9f72ca6fea856'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f0389be7-a6e2-400a-ba59-1227dfc8c678'
+};
